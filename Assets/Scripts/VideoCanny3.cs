3939e8b90e7bee5c44465140cebaf6a9688fd827
Long Qian <joewalker.ql@gmail.com>
2016-06-09 16:43:28 -0400

    commented document

--- Assets/Scripts/VideoCanny3.cs
+++ Assets/Scripts/VideoCanny3.cs
@@ -25,10 +25,14 @@ using System.Collections;
 using System.Collections.Generic;
 using OpenCvSharp;
 
+// Parallel computation support
 using Uk.Org.Adcock.Parallel;
 using System;
 using System.Runtime.InteropServices;
 
+
+// class for video display and processed video display
+// current process is canny edge detection
 public class VideoCanny3 : MonoBehaviour
 {
 
@@ -37,45 +41,55 @@ public class VideoCanny3 : MonoBehaviour
     public MeshRenderer ProcessedTextureRenderer;
     public int deviceNumber;
     private WebCamTexture _webcamTexture;
+
+    // Video size
     private const int imWidth = 1280;
     private const int imHeight = 720;
     private int imFrameRate;
 
-
+    // OpenCVSharp parameters
     private Mat videoSourceImage;
     private Mat cannyImage;
     private Texture2D processedTexture;
     private Vec3b[] videoSourceImageData;
     private byte[] cannyImageData;
+
+    // Frame rate parameter
     private int updateFrameCount = 0;
     private int textureCount = 0;
     private int displayCount = 0;
 
     void Start() {
 
+        // create a list of webcam devices that is available
         WebCamDevice[] devices = WebCamTexture.devices;
         
         if (devices.Length > 0) { 
 
+            // initialized the webcam texture by the specific device number
             _webcamTexture = new WebCamTexture(devices[deviceNumber].name, imWidth, imHeight);
-
+            // assign webcam texture to the meshrenderer for display
             WebCamTextureRenderer.material.mainTexture = _webcamTexture;
 
             // Play the video source
             _webcamTexture.Play();
 
+            // initialize video / image with given size
             videoSourceImage = new Mat(imHeight, imWidth, MatType.CV_8UC3);
             videoSourceImageData = new Vec3b[imHeight * imWidth];
             cannyImage = new Mat(imHeight, imWidth, MatType.CV_8UC1);
             cannyImageData = new byte[imHeight * imWidth];
 
+            // create processed video texture as Texture2D object
             processedTexture = new Texture2D(imWidth, imHeight, TextureFormat.RGBA32, true, true);
 
+            // assign the processedTexture to the meshrenderer for display
             ProcessedTextureRenderer.material.mainTexture = processedTexture;
 
 
         }
 
+        // create opencv window to display the original video
         Cv2.NamedWindow("Copy video");
         
 
@@ -93,12 +107,14 @@ public class VideoCanny3 : MonoBehaviour
 
                 textureCount++;
 
+                // convert texture of original video to OpenCVSharp Mat object
                 TextureToMat();
-
+                // update the opencv window of source video
                 UpdateWindow(videoSourceImage);
-
+                // create the canny edge image out of source image
                 ProcessImage(videoSourceImage);
-
+                // convert the OpenCVSharp Mat of canny image to Texture2D
+                // the texture will be displayed automatically
                 MatToTexture();
 
             }
@@ -109,6 +125,7 @@ public class VideoCanny3 : MonoBehaviour
         }
 
 
+        // output frame rate information
         if (updateFrameCount % 30 == 0) {
             Debug.Log("Frame count: " + updateFrameCount + ", Texture count: " + textureCount + ", Display count: " + displayCount);
         }
@@ -117,10 +134,14 @@ public class VideoCanny3 : MonoBehaviour
     }
 
 
-    // Convert unity texture object ot opencv Mat object
+    // Convert Unity Texture2D object to OpenCVSharp Mat object
     void TextureToMat() {
+        // Color32 array : r, g, b, a
         Color32[] c = _webcamTexture.GetPixels32();
 
+        // Parallel for loop
+        // convert Color32 object to Vec3b object
+        // Vec3b is the representation of pixel for Mat
         Parallel.For(0, imHeight, i => {
             for (var j = 0; j < imWidth; j++) {
                 var col = c[j + i * imWidth];
@@ -129,20 +150,24 @@ public class VideoCanny3 : MonoBehaviour
                     Item1 = col.g,
                     Item2 = col.r
                 };
+                // set pixel to an array
                 videoSourceImageData[j + i * imWidth] = vec3;
             }
         });
+        // assign the Vec3b array to Mat
         videoSourceImage.SetArray(0, 0, videoSourceImageData);
     }
 
 
 
-    // Convert opencv Mat object to unity texture2D object
+    // Convert OpenCVSharp Mat object to Unity Texture2D object
     void MatToTexture() {
+        // cannyImageData is byte array, because canny image is grayscale
         cannyImage.GetArray(0, 0, cannyImageData);
-
+        // create Color32 array that can be assigned to Texture2D directly
         Color32[] c = new Color32[imHeight * imWidth];
 
+        // parallel for loop
         Parallel.For(0, imHeight, i => {
             for (var j = 0; j < imWidth; j++) {
                 byte vec = cannyImageData[j + i * imWidth];
@@ -155,7 +180,9 @@ public class VideoCanny3 : MonoBehaviour
                 c[j + i * imWidth] = color32;
             }
         });
+
         processedTexture.SetPixels32(c);
+        // to update the texture, OpenGL manner
         processedTexture.Apply();
     }
 
@@ -168,14 +195,14 @@ public class VideoCanny3 : MonoBehaviour
     }
 
 
-    // Display the original video in a CV window
+    // Display the original video in a opencv window
     void UpdateWindow(Mat _image) {
         Cv2.Flip(_image, _image, FlipMode.X);
         Cv2.ImShow("Copy video", _image);
         displayCount++;
     }
     
-
+    // close the opencv window
     public void OnDestroy() {
         Cv2.DestroyAllWindows();
 
